4958faf305b0dc57ca689707680af3702b2dfa9f
youngG209 <young209@mcocoa.com>
2020-12-01 15:57:55 +0900

    Spring Batch - Database Writer 1) Custom ItemWriter  1. Reader에서 읽어온 데이터를 RestTemplate으로 외부 API로 전달해야할때  2. 임시저장을 하고 비교하기 위해 싱글톤 객체에 값을 넣어야할때  3. 여러 Entity를 동시에 save 해야할때  4. 상기 상황으로 인해 직접 구현하기 위해 -> ItemWriter 인터페이스 사용하여 구현

---
+++ src/main/java/com/springbatchdemo/writer/CustomItemWriterJobConfiguration.java
@@ -0,0 +1,73 @@
+package com.springbatchdemo.writer;
+
+import com.springbatchdemo.entity.pay.Pay;
+import com.springbatchdemo.entity.pay.Pay2;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+public class CustomItemWriterJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    private static final int chunkSize = 10;
+
+    public CustomItemWriterJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    public Job customItemWriterJob() {
+        return jobBuilderFactory.get("customItemWriterJob")
+                .start(customItemWriterStep())
+                .build();
+    }
+
+    @Bean
+    public Step customItemWriterStep() {
+        return stepBuilderFactory.get("customItemWriterStep")
+                .<Pay, Pay2>chunk(chunkSize)
+                .reader(customItemWriterReader())
+                .processor(customItemWriterProcessor())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Pay> customItemWriterReader() {
+        return new JpaPagingItemReaderBuilder<Pay>()
+                .name("customItemWriterReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                .queryString("select p from Pay p")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Pay, Pay2> customItemWriterProcessor() {
+        return pay -> new Pay2(pay.getAmount(), pay.getTxName(), pay.getTxDateTime());
+    }
+
+    @Bean
+    public ItemWriter<Pay2> customItemWriter() {
+        return items -> {
+            for (Pay2 pay : items) {
+                System.out.println(pay);
+            }
+        };
+    }
+}

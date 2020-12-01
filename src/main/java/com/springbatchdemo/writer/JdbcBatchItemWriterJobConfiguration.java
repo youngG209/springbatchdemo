feedcd153274bbf984af18d07e9274fb6c5374b1
youngG209 <young209@mcocoa.com>
2020-12-01 12:10:44 +0900

    Spring Batch - Database Writer 1) JdbcBatchItemWriter  1. JDBC의 Batch 기능을 사용하여 한번에 Database로 Query 전달하여 Database 내부에서 Query들이 실행 됨  2. columnMapped : Map 타입 , beanMapped : Pay.class와 같은 Pojo 타입

---
+++ src/main/java/com/springbatchdemo/writer/JdbcBatchItemWriterJobConfiguration.java
@@ -0,0 +1,72 @@
+package com.springbatchdemo.writer;
+
+import com.springbatchdemo.entity.pay.Pay;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcBatchItemWriterJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource; // DataSource DI
+
+    private static final int chunkSize = 10;
+
+    public JdbcBatchItemWriterJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public Job jdbcBatchItemWriterJob() {
+        return jobBuilderFactory.get("jdbcBatchItemWriterJob")
+                .start(jdbcBatchItemWriterStep())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcBatchItemWriterStep() {
+        return stepBuilderFactory.get("jdbcBatchItemWriterStep")
+                .<Pay, Pay>chunk(chunkSize)
+                .reader(jdbcBatchItemWriterReader())
+                .writer(jdbcBatchItemWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<? extends Pay> jdbcBatchItemWriterReader() {
+        return new JdbcCursorItemReaderBuilder<Pay>()
+                .fetchSize(chunkSize)
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+                .sql("select id, amount, tx_name, tx_date_time from pay")
+                .name("jdbcBatchItemWriter")
+                .build();
+    }
+
+    /**
+     * reader에서 넘어온 데이터를 하나씩 출력하는 writer
+     * @return
+     */
+    @Bean // beanMapped()을 사용할때는 필수로 @Bean 설정
+    public JdbcBatchItemWriter<Pay> jdbcBatchItemWriter() {
+        return new JdbcBatchItemWriterBuilder<Pay>()
+                .dataSource(dataSource)
+                .sql("insert into pay2(amount, tx_name, tx_date_time) values (:amount, :txName, :txDateTime)")
+                .beanMapped()
+                .build();
+    }
+}

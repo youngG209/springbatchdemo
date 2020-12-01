eaf9cd148517e387df618b4f7c7e8e3629686773
youngG209 <young209@mcocoa.com>
2020-12-01 10:46:51 +0900

    Spring Batch - JpaPagingItemReader 1. JPA를 사용 2. EntityManagerFactory를 지정하는 것 외에 JdbcPagingItemReader와 크게 다른 점 X
    ***
    JpaPagingItemReader 주의 사항
    1. 정렬 꼭 필요(order by)
    2. JPA에는 Cursor 기반 Database 접근을 지원하지 X

---
+++ src/main/java/com/springbatchdemo/reader/jdbc/JpaPagingItemReaderJobConfiguration.java
@@ -0,0 +1,69 @@
+package com.springbatchdemo.reader.jdbc;
+
+import com.springbatchdemo.entity.pay.Pay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+public class JpaPagingItemReaderJobConfiguration {
+
+    private static Logger log = LoggerFactory.getLogger(JpaPagingItemReaderJobConfiguration.class);
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    private int chunkSize = 10;
+
+    public JpaPagingItemReaderJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    public Job jpaPagingItemReaderJob() {
+        return jobBuilderFactory.get("jpaPagingItemReaderJob")
+                .start(jpaPagingItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step jpaPagingItemReaderStep() {
+        return stepBuilderFactory.get("jpaPagingItemReaderStep")
+                .<Pay, Pay>chunk(chunkSize)
+                .reader(jpaPagingItemReader())
+                .writer(jpaPaingItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Pay> jpaPagingItemReader() {
+        return new JpaPagingItemReaderBuilder<Pay>()
+                .name("jpaPagingItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                .queryString("select p from Pay p where amount >= 2000")
+                .build();
+    }
+
+    private ItemWriter<Pay> jpaPaingItemWriter() {
+        return list -> {
+            for (Pay pay : list) {
+                log.info("Current Pay = {} ",pay);
+            }
+        };
+    }
+}
